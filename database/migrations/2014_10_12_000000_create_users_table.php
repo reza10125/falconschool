a6cfa071f748aef8311f8563d5562a3d2a4a43aa
rajin <mofakharulislamcse11@gmail.com>
2023-02-18 21:41:09 +0600

    18-2-2023

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,30 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->engine = "InnoDB";
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->string('password');
+                $table->date('dob')->nullable();
+                $table->date('join_date')->nullable();
+                $table->boolean('g_auth')->default(0)->comment('Google 2 step 1 for enable, 0 for disable');
+                $table->boolean('email_auth')->default(0)->comment('Email auth 1 for enable, 0 for disable');
+                $table->string('secret_key', 64)->nullable();
+                $table->string('phone');
+                $table->tinyInteger('user_type')->nullable();
+                $table->string('last_login_date');
+                $table->string('last_login_ip');
+                $table->timestamp('email_verified_at')->nullable();
+                $table->unsignedBigInteger('parent_id')->nullable()->comment('FK:parent_id(id)');
+                $table->index('parent_id');
+                $table->foreign('parent_id')->references('id')->on('parents')->onDelete('cascade');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
